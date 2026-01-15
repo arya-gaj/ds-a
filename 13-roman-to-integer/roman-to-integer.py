c40bf9ce5a9a1dad5a470f9d3eaebde26669897b
Aryaman Gajrani <ugs22091_it.aryaman@cbit.org.in>
2026-01-15 21:21:55 +0530

    Time: 2 ms (84.63%) | Memory: 19.2 MB (13.63%) - LeetSync

---
+++ 13-roman-to-integer/roman-to-integer.py
@@ -0,0 +1,19 @@
+class Solution:
+    def romanToInt(self, s: str) -> int:
+        roman = {
+            'I': 1, 'V': 5, 'X': 10, 'L': 50,
+            'C': 100, 'D': 500, 'M': 1000
+        }
+        
+        total = 0
+        prev = 0
+        
+        for char in reversed(s):
+            val = roman[char]
+            if val < prev:
+                total -= val
+            else:
+                total += val
+            prev = val
+        
+        return total
